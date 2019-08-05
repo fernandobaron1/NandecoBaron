9885878d63ea1199bd8405e669aa565a9c7ceb97
Fernando Baron <fernandobaron1@hotmail.com>
2019-08-04 21:14:39 -0300

    Modelo 1

---
+++ NandecoBaron/src/nandecobaron/NandecoBaron.java
@@ -0,0 +1,23 @@
+
+package nandecobaron;
+
+
+public class NandecoBaron {
+
+    
+    public static void main(String[] args) {
+        
+        int num1, num2, num3;
+        
+        num1 = 100;
+        num2 = 100;
+        num3 = 100;
+        
+        int resultado;
+        resultado = (num1 + num2 + num3)/3;
+        System.out.println("O resultado é "+resultado);
+    }
+    
+}
+    
+    
